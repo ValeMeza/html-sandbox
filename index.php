c220e1f3599c612059a8f24e01368025abcdb02f
Valente Meza <kingracoon64@gmail.com>
2017-01-10 16:00:25 -0700

    forgot to add <body>

--- index.php
+++ index.php
@@ -2,9 +2,47 @@
 <html lang="en">
     <head>
         <meta charset="utf-8"/>
-        <title>HTML SANDBOX</title>
     </head>
-    <body>
+    <header><h1>This is a FUCKING DEMO PAGE!</h1></header>
+
+        <!-- start slipsum code -->
+
+<body>
+    <main><p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
+
+        <!-- end slipsum code -->
+        <!-- start slipsum code -->
+
+        Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that <em><strong>shit.</strong></em></p></main>
+        <h2> A few of Sam's Favorite Things</h2>
+    <ul>
+        <li>Keeping Cool.</li>
+        <li>Planes with no snakes.</li>
+        <li>civil rights.</li>
+    </ul>
+
+        <!-- end slipsum code -->
+
+    <h3>This is a Table Bitch</h3>
+    <table>
+        <tr>
+        <th>Column 1</th>
+        <th>Column 2</th>
+        <!-- row 1 -->
+
+        <tr>
+            <td>Snakes on a plane</td>
+            <td>Jackie Brown</td>
+        </tr>
+        <tr>
+            <td>Hateful 8</td>
+            <td>Pulp Fiction</td>
+        </tr>
+    </table>
+
+        <img src="images/Sam-fucking-jack.jpg" alt="Sam-fucking-jack"/a>
+    <p>This is a link to <a href="https://google.com" target="_blank"</p>
+
 
     </body>
 </html>
